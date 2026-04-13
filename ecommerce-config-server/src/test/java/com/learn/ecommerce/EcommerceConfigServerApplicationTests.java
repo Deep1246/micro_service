503dc70898921404f187803ed3515f6c924aa18b
unknown <sandeep314yadav@gmail.com>
2026-04-13 19:27:01 +0530

    centalized config

--- ecommerce-config-server/src/test/java/com/learn/ecommerce/EcommerceConfigServerApplicationTests.java
+++ ecommerce-config-server/src/test/java/com/learn/ecommerce/EcommerceConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.microservice.api_gateway.api_gateway;
+package com.learn.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class EcommerceConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
